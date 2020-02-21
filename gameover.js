6a51b6d095c13500c6c53316a502ddaa342d7a17
allrounder27 <gautambafna2708@gmail.com>
2020-02-22 02:13:52 +0530

    minor fixes

--- gameover.js
+++ gameover.js
@@ -1,14 +1,3 @@
-// function loadDoc() {
-//   var xhttp = new XMLHttpRequest();
-//   xhttp.onreadystatechange = function() {
-//     if (this.readyState == 4 && this.status == 200) {
-//       window.location.href = "index.html";
-//     }
-//   };
-//   xhttp.open("GET", "index.html", true);
-//   xhttp.send();
-// }
-
 function draw() {
   var ctx = document.getElementById('canvas').getContext('2d');
   ctx.font = '48px serif';
